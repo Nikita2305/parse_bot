8f2c1e74418ba7e8f55ce137af100cad0a91fa48
root <root@194-58-103-47.cloudvps.regruhosting.ru>
2023-03-12 16:36:24 +0300

    fix: Change directory in login.py

--- demhack/login.py
+++ demhack/login.py
@@ -2,10 +2,12 @@ import argparse
 import getpass
 from pprint import pprint
 import json
+import os
 
 from telegram_simple.client import Telegram, AuthorizationState
 
 DATABASE_ENC_KEY = "vk_parse_bot_1234"
+CODE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/demhack"
 
 def main(config_path):
     with open(config_path, "r") as f:
@@ -17,11 +19,14 @@ def main(config_path):
         if "password" in config:
             PASSWORD = config["password"]
 
+    print(PHONE)
+
     tg = Telegram(
         api_id=API_ID,
         api_hash=API_HASH,
         phone=PHONE,
         database_encryption_key=DATABASE_ENC_KEY,
+        files_directory=f'{CODE_PATH}/.tdlib_files/'
     )
 
     # you must call login method before others
@@ -32,7 +37,7 @@ def main(config_path):
     if state == AuthorizationState.WAIT_CODE:
         # print("Pin is required. In this example, the main program is asking it, not the python-telegram client")
         pin = input("Please insert pin code here: ")
-        print("In this example, the main program is more polite than the python-telegram client")
+        # print("In this example, the main program is more polite than the python-telegram client")
         tg.send_code(pin)
         state = tg.login(blocking=False)
 
